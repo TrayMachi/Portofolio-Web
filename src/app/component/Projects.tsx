3e28aa85cd2ac6269b0566443d7437eb4de5a5d1
TrayMachi <tristanagraa@gmail.com>
2024-03-21 12:06:51 +0700

    Feat: New Project added

--- src/app/component/Projects.tsx
+++ src/app/component/Projects.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useScreenSize } from "../utils/screensize";
 import { motion } from "framer-motion";
 import Skills from "./Skills";
+import { RxExternalLink } from "react-icons/rx";
 
 interface Post {
   id: number;
@@ -11,6 +12,7 @@ interface Post {
   img: string;
   desc: string;
   ref: string;
+  demo: string;
 }
 
 interface Categories {
@@ -33,15 +35,17 @@ const Projects = () => {
         img: "/Project/project1.png",
         desc: "This is my personal portfolio website (yes, you're currently on it), built using Next.js, Typescript, Tailwind CSS, and other external libraries.",
         ref: "https://github.com/TrayMachi/Portofolio-Web",
+        demo: "",
       },
     ],
     project2: [
       {
         id: 2,
-        altImg: "Template",
+        altImg: "Expense Tracker Website",
         img: "/Project/project2.png",
-        desc: "Cuman template banh, saya kurang project :')",
-        ref: "",
+        desc: "Web app that help you track your income & expense. Built using Next.js, AppWrite BaaS, and Shadcn UI.",
+        ref: "https://github.com/TrayMachi/money-web",
+        demo: "https://money-managements.vercel.app/",
       },
     ],
   });
@@ -78,30 +82,43 @@ const Projects = () => {
                       {post.altImg}
                     </h5>
                   </a>
-                  <p className="mb-3 font-normal text-gray-400 text-justify">{post.desc}</p>
-                  <motion.a
-                    whileTap={{ scale: 0.85 }}
-                    href={post.ref}
-                    target="_blank"
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg bg-[#904edb] hover:bg-[#632f77] active:bg-[#7d3699] text-[#0d0e0d] hover:scale-105 transform transition duration-300"
-                  >
-                    Read more
-                    <svg
-                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
+                  <p className="mb-3 font-normal text-gray-400 text-justify">
+                    {post.desc}
+                  </p>
+                  <div className="flex items-center space-x-4">
+                    <motion.a
+                      whileTap={{ scale: 0.85 }}
+                      href={post.ref}
+                      target="_blank"
+                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg bg-[#904edb] hover:bg-[#632f77] active:bg-[#7d3699] text-[#0d0e0d] hover:scale-105 transform transition duration-300"
                     >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </motion.a>
+                      Read more
+                      <svg
+                        className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                      </svg>
+                    </motion.a>
+                    <motion.a
+                      whileTap={{ scale: 0.85 }}
+                      href={post.demo}
+                      target="_blank"
+                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg bg-[#b33e95] hover:bg-[#832e6e] active:bg-[#fcd7f3] text-[#0d0e0d] hover:scale-105 transform transition duration-300"
+                    >
+                      <p className="px-1">Live Demo</p>
+                      <RxExternalLink />
+                    </motion.a>
+                  </div>
                 </div>
               </div>
             ))
